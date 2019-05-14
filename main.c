fb6f370affb175f658636457aa79b7890175a080
Natália <natalia.gama.mattos@hotmail.com>
2019-05-14 12:56:12 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+int vet[10]= {1,2,4,2,4,6,7,10,8,9};
+int aux[100];
+int cont = 0;
+int n = 0;
+int tam_aux = 100;
+int tam_vet = 10;
+
+for(int i = 0; i< tam_aux;i++){
+  aux[i] = 0;
+}
+
+for(int j = 0; j< tam_vet;j++){
+  aux[vet[j]]+=1;
+}
+
+while(cont<10){
+  if(aux[n]== 0)
+    n++;
+  else{
+    vet[cont] = n;
+    aux[n]--;
+    cont++;
+  }
+}
+
+for (int k = 0; k<tam_vet;k++){
+    printf("%d ", vet[k]);
+}
+return 0;
+}
+ 
